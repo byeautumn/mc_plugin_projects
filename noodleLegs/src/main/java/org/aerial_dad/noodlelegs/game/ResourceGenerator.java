88e664cd0f9a4ecb5d97215606fa75d190cb0520
Alexander Gao <alexgao@Alexanders-Mini.lan>
2024-11-23 18:36:28 -0500

    Added ResourceGenerator.

---
+++ noodleLegs/src/main/java/org/aerial_dad/noodlelegs/game/ResourceGenerator.java
@@ -0,0 +1,26 @@
+package org.aerial_dad.noodlelegs.game;
+
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+
+public class ResourceGenerator {
+
+    private static Player operator;
+
+    private Map<Material, Integer> materialGenerated;
+
+    private int interval;
+
+    private Location generatiorLocation;
+
+    public void Generator(Player itemReceiver){
+
+        
+
+    }
+
+}
