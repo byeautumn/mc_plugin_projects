689ef446009e993f5dd873735e74bc293d4f3658
BuildTools <unconfigured@null.spigotmc.org>
2024-11-23 19:00:42 -0500

    Added something in ResourceGenerator.

--- noodleLegs/src/main/java/org/aerial_dad/noodlelegs/game/ResourceGenerator.java
+++ noodleLegs/src/main/java/org/aerial_dad/noodlelegs/game/ResourceGenerator.java
@@ -3,8 +3,11 @@ package org.aerial_dad.noodlelegs.game;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ResourceGenerator {
@@ -17,10 +20,30 @@ public class ResourceGenerator {
 
     private Location generatiorLocation;
 
+    private final static double RECEIVER_RADIUS = 5.0d;
+
     public void Generator(Player itemReceiver){
 
         
 
+    }
+
+    private void generate() {
+        World world = generatiorLocation.getWorld();
+        List<Player> receivers = new ArrayList<>();
+        for (Player player : world.getPlayers()) {
+            Location playerLocation = player.getLocation();
+            double distance = this.generatiorLocation.distance(playerLocation);
+            if (distance <= RECEIVER_RADIUS) {
+                receivers.add(player);
+            }
+        }
+
+        if (receivers.isEmpty()) {
+
+        } else {
+            
+        }
     }
 
 }
